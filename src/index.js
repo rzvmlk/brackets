b44d908d9497802e99d676461189c6dae7337f77
Nadia Halkina <nadia.esperanza.2000@gmail.com>
2023-01-26 16:55:08 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      if(str.length%2!=0){
+        return false;
+    }
+    let arr=str.split('');
+    let stack=[];
+    for(let j=0;j<arr.length; j++){
+        for(let i=0; i<bracketsConfig.length;i++){
+            if(stack[stack.length-1]==bracketsConfig[i][0]&&arr[j]==bracketsConfig[i][1]){
+                stack.pop();
+            }else{
+                if(arr[j]==bracketsConfig[i][0]){
+                    stack.push(arr[j]);
+                }
+            }
+        }
+
+    }
+    return stack.length==0;
 }
